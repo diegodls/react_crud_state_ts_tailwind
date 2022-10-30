71ef3e3d430baa53bbab830718a3f9ffe1ec8afe
Diego <contato.diegoluisdasilva@gmail.com>
2022-10-30 10:54:36 -0300

    First commit

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,40 @@
+import { Transition } from "@headlessui/react";
+import { TaskProps } from "../interfaces/interfaces";
+
+interface TasksListProps {
+  task: TaskProps;
+  handleRemoveItem: (id: number) => void;
+}
+
+export function ListItem(props: TasksListProps) {
+  const { handleRemoveItem } = props;
+  const { id, task } = props.task;
+
+  return (
+    <Transition
+      appear={true}
+      unmount={true}
+      show={true}
+      enter='transition-opacity duration-150'
+      enterFrom='opacity-0'
+      enterTo='opacity-100'
+      leave='transition-opacity duration-75'
+      leaveFrom='opacity-100'
+      leaveTo='opacity-0'
+    >
+      <li
+        className='w-full h-16 flex items-center justify-between p-2 bg-zinc-800 border-b border-b-green-500'
+        key={id}
+      >
+        <p className='font-bold text-white'>{task}</p>
+
+        <button
+          className='font-bold p-2 mr-2 flex items-center justify-center rounded-md bg-red-500 text-md text-white'
+          onClick={() => handleRemoveItem(id)}
+        >
+          X
+        </button>
+      </li>
+    </Transition>
+  );
+}
---
+++ src/components/TasksList.tsx
@@ -0,0 +1,34 @@
+import { CompletedList } from "../assets/svg/CompletedList";
+import { TaskProps } from "../interfaces/interfaces";
+import { ListItem } from "./ListItem";
+
+interface TasksListProps {
+  allTasks: TaskProps[];
+  handleRemoveItem: (id: number) => void;
+}
+
+export function TasksList(props: TasksListProps) {
+  const { allTasks, handleRemoveItem } = props;
+
+  return (
+    <div
+      className='w-full h-full rounded-md bg-zinc-700 overflow-hidden 
+    scrollbar-thumb-green-600 scrollbar-track-transparent scrollbar-thin'
+    >
+      {allTasks && allTasks.length > 0 ? (
+        <ul>
+          {allTasks.map((task: TaskProps) => (
+            <ListItem task={task} handleRemoveItem={handleRemoveItem} />
+          ))}
+        </ul>
+      ) : (
+        <div className='w-full h-full flex flex-col items-center justify-center'>
+          <CompletedList width={200} height={200} />
+          <p className='text-green-500'>
+            Parabéns, todas as tarefas foram completadas!
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
